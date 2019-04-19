dd570f895b0fedde6e508c041d7f9f6071a1c8e4
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-19 14:35:54 +0900

    group_sakusei
    グループ作成.8まで

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user   
@@ -17,10 +20,19 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
   def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
