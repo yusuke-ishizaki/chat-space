ecaacd2eb772167639bce4797e0f7f3231df7203
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-22 16:21:47 +0900

    change3
    form属性のresetメソッドの使用
    インデントずれ
    BEM記法
    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,55 +1,49 @@
 $(function () {
-    function buildHTML(message) {
-        var img =(message.image.url)? img = `<img src = ${message.image.url} class: "lower-message_image">`:"";
-
-        var html =
-            `<div class="message">
-              <div class="upper-message">
-                <div class="upper-message__user-name">
-                  ${ message.name}
-                </div>
-                <div class="upper-message__date">
-                  ${ message.date}
-                </div>
-              </div>
-              <div class="lower-meesage">
-                <p class="lower-message__content">
-                  ${ message.content}
-                </p>
-                  ${img}
-              </div>
-            </div>`;
-        return html;
-    }
-    function scroll_view() {
-        $('.main__bottom') .animate({scrollTop: $('.main__bottom')[0].scrollHeight},'fasts')
-    }
-
-
-    $('#new_message').on('submit', function (e) {
-        e.preventDefault();
-        var formData = new FormData(this);
-        var url = $(this).attr("action");
-
-        $.ajax({
-            url: url,
-            type: "POST",
-            data: formData,
-            dataType: 'json',
-            processData: false,
-            contentType: false
-        })
-
-        .done(function (data) {
-            var html = buildHTML(data);
-            $('.main__bottom').append(html);
-            scroll_view()
-            $(".form__submit").prop("disabled", false); 
-            $('#message_content').val("");
-            $('.form__textfield').val('');
-        })
-        .fail(function () {
-            alert('error');
-        });
+  function buildHTML(message) {
+    var img =(message.image.url)? img = `<img src = ${message.image.url} class: "lower-message_image">`:"";  
+    var html =
+      `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${ message.name}
+          </div>
+          <div class="upper-message__date">
+            ${ message.date}
+          </div>
+        </div>
+        <div class="lower-meesage">
+          <p class="lower-message__content">
+            ${ message.content}
+          </p>
+            ${img}
+        </div>
+      </div>`;
+    return html;
+  }
+  function scroll_view() {
+    $('.main__bottom') .animate({scrollTop: $('.main__bottom')[0].scrollHeight},'fasts')
+  }  
+  $('#new_message').on('submit', function (e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr("action");  
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })  
+    .done(function (data) {
+      var html = buildHTML(data);
+      $('.main__bottom').append(html);
+      scroll_view()
+      $(".form__submit").prop("disabled", false); 
+      $("form")[0].reset();
     })
+    .fail(function () {
+      alert('error');
+    });
+  })
 }) 
