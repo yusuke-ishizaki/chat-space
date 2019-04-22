9051546a66514c39e1983c01ff065343cb29e320
yusuke-ishizaki <isizaki1114@gmail.com>
2019-04-22 16:39:16 +0900

    change4
    &使用
    インデントずれ
    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
 
   def update
     if current_user.update(user_params)
-        redirect_to root_path
-      else
-        render :edit
-      end
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
